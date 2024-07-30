3e2328581857de48414609d054aee05aa1baa3de
Schnirivadze <andriiseleznovukr@gmail.com>
2024-07-30 17:13:16 +0200

    [fix] overflowing contents (#1)

--- src/pages/main/renderer.js
+++ src/pages/main/renderer.js
@@ -1,6 +1,6 @@
 var currentFolder = "/home/veniamin/Projects"
 
-document.addEventListener("DOMContentLoaded", () => { loadDirectory("/home/veniamin/Projects") });
+document.addEventListener("DOMContentLoaded", () => { loadDirectory(currentFolder) });
 document.getElementById("window-button-minimize").addEventListener("click", () => { window.electronAPI.minimize() });
 document.getElementById("window-button-maximize").addEventListener("click", () => { window.electronAPI.maximize() });
 document.getElementById("window-button-close").addEventListener("click", () => { window.electronAPI.close() });
